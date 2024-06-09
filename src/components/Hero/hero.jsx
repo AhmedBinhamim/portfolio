6ae87dcc1adb027e6f3f61d1127baf15068c688e
AhmedBinhamim <ahmedbinhamim@gmail.com>
2024-06-09 14:53:26 +0300

    updated hero

--- src/components/Hero/hero.jsx
+++ src/components/Hero/hero.jsx
@@ -1,32 +1,65 @@
 import React from 'react';
+import { TypeAnimation } from 'react-type-animation';
+import { FaFacebook, FaTwitter, FaYoutube } from 'react-icons/fa'; // Import Font Awesome icons
 import styles from './hero.module.css';
-import { getImageUrl } from '../../utilis'; // Assuming this imports the getImageUrl function correctly
+import { getImageUrl } from '../../utilis';
 
 const Hero = () => {
-  // Assuming getImageUrl returns the local image URL
-  const localImageUrl = getImageUrl('Hero/hero-bg.png'); // Use your getImageUrl function to get the local image URL
-  
+  const heroImageUrl = getImageUrl('Hero/hero-background.jpg');
+
   return (
-    <div 
-      className="p-5 text-center bg-image rounded-3" 
-      style={{
-        backgroundImage: `url(${localImageUrl})`, // Set local image URL dynamically
-        height: '600px'
-      }}
-    >
-      <div className="mask" style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)' }}>
-        <div className="d-flex justify-content-center align-items-center h-100">
-          <div className="text-white">
-            <h1 className="mb-3">Heading</h1>
-            <h4 className="mb-3">Subheading</h4>
-            <a data-mdb-ripple-init className="btn btn-outline-light btn-lg" href="#!" role="button">
-              Call to action
-            </a>
+    <div className={styles.heroContainer}>
+      <div className="row py-5">
+        <div className="col-lg-1 d-none d-lg-flex align-items-center">
+          <div className="lc-block d-flex" style={{ writingMode: 'vertical-rl', transform: 'rotate(360deg)' }}>
+            <a className="btn btn-link my-2 text-dark text-decoration-none" href="#" role="button"><i class="fa-brands fa-github"  style={{ fontSize: '32px' }}></i></a>
+            <a className="btn btn-link my-4 text-dark text-decoration-none" href="#" role="button"><i class="fa-brands fa-linkedin-in"  style={{ fontSize: '32px' }}></i></a>
+            <a className="btn btn-link my-2 text-dark text-decoration-none" href="#" role="button"><i class="fa-solid fa-envelope"  style={{ fontSize: '32px' }}></i></a>
+          </div>
+        </div>
+        <div className="col-sm-6 d-flex d-lg-none flex-sm-column align-items-center justify-content-around mb-4 mb-sm-0">
+          <div className="lc-block">
+            <a className="btn btn-link text-dark text-decoration-none" href="#" role="button"><i class="fa-brands fa-github"  style={{ fontSize: '32px' }}></i></a>
+          </div>
+          <div className="lc-block">
+            <a className="btn btn-link text-dark text-decoration-none" href="#" role="button"><i class="fa-brands fa-linkedin-in"  style={{ fontSize: '32px' }}></i></a>
+          </div>
+          <div className="lc-block">
+            <a className="btn btn-link text-dark text-decoration-none" href="#" role="button"><i class="fa-solid fa-envelope"  style={{ fontSize: '32px' }}></i></a>
+          </div>
+        </div>
+        <div className="col-lg-4 col-sm-6">
+          <div className="lc-block ratio ratio-1x1">
+            <img
+              style={{ objectFit: 'cover' }}
+              className="img-fluid"
+              src={getImageUrl('Hero/hero-bg.png')}
+              alt="Photo by Atikh Bana"
+            />
+          </div>
+        </div>
+        <div className="col-lg-4 offset-lg-1" >
+          <div className={styles.content}>
+          <div className="lc-block" >
+            <div editable="rich">
+            <h2 className="rfs-25">Hi, I am Ahmed</h2>
+            </div>
+          </div>
+          </div>
+          <div className="lc-block">
+            <div editable="rich">
+              <p>Bachelor of Computer Science - Software Engineering graduate from Multimedia University, Malaysia with
+                a strong passion for software development. Skilled in various programming languages ranging from Front-end development to Back-end development Frameworks. Active Community service participant served as
+                𝗠𝗶𝗰𝗿𝗼𝘀𝗼𝗳𝘁 𝗟𝗲𝗮𝗿𝗻 𝗦𝘁𝘂𝗱𝗲𝗻𝘁 𝗔𝗺𝗯𝗮𝘀𝘀𝗮𝗱𝗼𝗿 and contributed to various community events.</p>
+            </div>
+          </div>
+          <div className="lc-block">
+            <a className={`btn btn-primary btn-lg ${styles.customButton}`} href="#" role="button">Contact Us</a>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Hero;
