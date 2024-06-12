203cc294c91384f1cc8bdee012b7b12191fa4f76
AhmedBinhamim <ahmedbinhamim@gmail.com>
2024-06-12 21:17:23 +0300

    added skills

--- src/components/Homepage/homepage.jsx
+++ src/components/Homepage/homepage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../Navbar/navbar';
 import Hero from '../Hero/hero';
+import Skills from '../Skills/skills';
 
 const Homepage = () => {
   return (
     <>
     <Navbar/>
     <Hero/>
+    <Skills/>
     </>
   )
 }
