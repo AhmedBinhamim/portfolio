425bf127d5bf5f0260e8f8038a1a1af9821bf142
Ahmed Hassan Binhamim <126442288+AhmedBinhamim@users.noreply.github.com>
2024-09-24 20:21:14 +0300

    new projects uploads

--- src/components/Projects/projects.jsx
+++ src/components/Projects/projects.jsx
@@ -10,6 +10,9 @@ const Projects = () => {
   const cpuProject = getImageUrl('Projects/CPU-project.png')
   const hospitalProject = getImageUrl('Projects/Hospital-project.png')
   const healthProject = getImageUrl('Projects/MHealth-project.png')
+  const fastFoodProject = getImageUrl('Projects/fast-food.png')
+  const liveChatProject = getImageUrl('Projects/live-chat.png')
+  const blogProject = getImageUrl('Projects/blog-project.png')
 
   return (
     <>
@@ -30,6 +33,7 @@ const Projects = () => {
 
         <div className="container overflow-hidden">
           <div className="row gy-4 gy-xxl-5">
+
             <div className="col-12 col-md-6 col-lg-4 d-flex">
               <article className="d-flex">
                 <div className="card border border-dark" style={{ "--bs-card-border-radius": "0", "--bs-card-inner-border-radius": "0" }}>
@@ -83,6 +87,94 @@ const Projects = () => {
                     </div>
                     <p className="card-text entry-summary text-secondary m-0 p-0 text-black" style={{ textAlign: 'justify' }}>
                      Developed and Designed website for AlShurfan Advanced Real Estate. The project was aiming to give brand identity and provide insights about services and projects provided by the organisation
+                    </p>
+                  </div>
+                </div>
+              </article>
+            </div>
+            
+            
+            <div className="col-12 col-md-6 col-lg-4 d-flex">
+              <article className="d-flex">
+                <div className="card border border-dark" style={{ "--bs-card-border-radius": "0", "--bs-card-inner-border-radius": "0" }}>
+                  <figure className="card-img-top m-0 overflow-hidden bsb-overlay-hover">
+                    <a href="https://github.com/AhmedBinhamim/fast-food-delivery-app" target='blank'>
+                      <img className="img-fluid bsb-scale bsb-hover-scale-up" loading="lazy" src={fastFoodProject} alt="Chain" />
+                    </a>
+                    <figcaption>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-eye text-white bsb-hover-fadeInDown" viewBox="0 0 16 16">
+                        <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
+                        <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
+                      </svg>
+                      <h4 className="h6 text-white bsb-hover-fadeInUp mt-2">View Project</h4>
+                    </figcaption>
+                  </figure>
+                  <div className="card-body border-0 bg-white p-4">
+                    <div className="entry-header mb-3">
+                      <h2 className="card-title entry-title h4 mb-0">
+                        <a className="link-dark link-opacity-100 link-opacity-75-hover text-decoration-none" href="https://github.com/AhmedBinhamim/fast-food-delivery-app" target='blank'>Fast Food Delivery Mobile App</a>
+                      </h2>
+                    </div>
+                    <p className="card-text entry-summary text-secondary m-0 p-0 text-black" style={{ textAlign: 'justify' }}>
+                     Developed, implemented and deployed a fast food delivery application built using Flutter and Firebase as backend service with integration of stripe as secure payment checkout.
+                    </p>
+                  </div>
+                </div>
+              </article>
+            </div>
+
+            <div className="col-12 col-md-6 col-lg-4 d-flex">
+              <article className="d-flex">
+                <div className="card border border-dark" style={{ "--bs-card-border-radius": "0", "--bs-card-inner-border-radius": "0" }}>
+                  <figure className="card-img-top m-0 overflow-hidden bsb-overlay-hover">
+                    <a href="https://github.com/AhmedBinhamim/Live-chat" target='blank'>
+                      <img className="img-fluid bsb-scale bsb-hover-scale-up" loading="lazy" src={liveChatProject} alt="Chain" />
+                    </a>
+                    <figcaption>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-eye text-white bsb-hover-fadeInDown" viewBox="0 0 16 16">
+                        <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
+                        <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
+                      </svg>
+                      <h4 className="h6 text-white bsb-hover-fadeInUp mt-2">View Project</h4>
+                    </figcaption>
+                  </figure>
+                  <div className="card-body border-0 bg-white p-4">
+                    <div className="entry-header mb-3">
+                      <h2 className="card-title entry-title h4 mb-0">
+                        <a className="link-dark link-opacity-100 link-opacity-75-hover text-decoration-none" href="https://github.com/AhmedBinhamim/Live-chat" target='blank'>Live Chat Application</a>
+                      </h2>
+                    </div>
+                    <p className="card-text entry-summary text-secondary m-0 p-0 text-black" style={{ textAlign: 'justify' }}>
+                     Developed and implemented a live chat application using NestJS as backend service with usage of websockets to ensure live communication. The frontend was built using Vue.JS with Typescript.
+                    </p>
+                  </div>
+                </div>
+              </article>
+            </div>
+
+            <div className="col-12 col-md-6 col-lg-4 d-flex">
+              <article className="d-flex">
+                <div className="card border border-dark" style={{ "--bs-card-border-radius": "0", "--bs-card-inner-border-radius": "0" }}>
+                  <figure className="card-img-top m-0 overflow-hidden bsb-overlay-hover">
+                    <a href="https://github.com/AhmedBinhamim/Blog-project" target='blank'>
+                      <img className="img-fluid bsb-scale bsb-hover-scale-up" loading="lazy" src={blogProject} alt="Chain" />
+                    </a>
+                    <figcaption>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-eye text-white bsb-hover-fadeInDown" viewBox="0 0 16 16">
+                        <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
+                        <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
+                      </svg>
+                      <h4 className="h6 text-white bsb-hover-fadeInUp mt-2">View Project</h4>
+                    </figcaption>
+                  </figure>
+                  <div className="card-body border-0 bg-white p-4">
+                    <div className="entry-header mb-3">
+                      <h2 className="card-title entry-title h4 mb-0">
+                        <a className="link-dark link-opacity-100 link-opacity-75-hover text-decoration-none" href="https://github.com/AhmedBinhamim/Blog-project" target='blank'>Blog Platform</a>
+                      </h2>
+                    </div>
+                    <p className="card-text entry-summary text-secondary m-0 p-0 text-black" style={{ textAlign: 'justify' }}>
+                     Developmed and implemented a Blog Writing Platform using NestJS as backend service. The front end was built using Angular. The project used Agile Methodology as an approach to built this project.
                     </p>
                   </div>
                 </div>
