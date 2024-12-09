377f53d98d6e54c3cd3c8f97b57beecfe59be642
Ahmed Hassan Binhamim <126442288+AhmedBinhamim@users.noreply.github.com>
2024-12-09 20:13:41 +0300

    added new projects

--- src/components/Projects/projects.jsx
+++ src/components/Projects/projects.jsx
@@ -13,6 +13,8 @@ const Projects = () => {
   const fastFoodProject = getImageUrl('Projects/fast-food.png')
   const liveChatProject = getImageUrl('Projects/live-chat.png')
   const blogProject = getImageUrl('Projects/blog-project.png')
+  const goHearingProject = getImageUrl('Projects/gohearing.png')
+  const kaawalaProject = getImageUrl('Projects/kaanwala.png')
 
   return (
     <>
@@ -92,6 +94,65 @@ const Projects = () => {
                 </div>
               </article>
             </div>
+
+            <div className="col-12 col-md-6 col-lg-4 d-flex">
+              <article className="d-flex">
+                <div className="card border border-dark" style={{ "--bs-card-border-radius": "0", "--bs-card-inner-border-radius": "0" }}>
+                  <figure className="card-img-top m-0 overflow-hidden bsb-overlay-hover">
+                    <a href="https://clinic.gohearing.in/" target='_blank'>
+                      <img className="img-fluid bsb-scale bsb-hover-scale-up" loading="lazy" src={goHearingProject} alt="Chain" />
+                    </a>
+                    <figcaption>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-eye text-white bsb-hover-fadeInDown" viewBox="0 0 16 16">
+                        <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
+                        <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
+                      </svg>
+                      <h4 className="h6 text-white bsb-hover-fadeInUp mt-2">View Project</h4>
+                    </figcaption>
+                  </figure>
+                  <div className="card-body border-0 bg-white p-4">
+                    <div className="entry-header mb-3">
+                      <h2 className="card-title entry-title h4 mb-0">
+                        <a className="link-dark link-opacity-100 link-opacity-75-hover text-decoration-none" href="https://clinic.gohearing.in/" target='_blank'>Go Hearing Clinic</a>
+                      </h2>
+                    </div>
+                    <p className="card-text entry-summary text-secondary m-0 p-0 text-black" style={{ textAlign: 'justify' }}>
+                    Developed an Appointment Booking System for Go-Hearing Clinic using Wordpress, Amelia plugin , and PHP. The appointment system allows appointment request, booking, confirmation and lab reports update
+                    </p>
+                  </div>
+                </div>
+              </article>
+            </div>
+
+            <div className="col-12 col-md-6 col-lg-4 d-flex">
+              <article className="d-flex">
+                <div className="card border border-dark" style={{ "--bs-card-border-radius": "0", "--bs-card-inner-border-radius": "0" }}>
+                  <figure className="card-img-top m-0 overflow-hidden bsb-overlay-hover">
+                    <a href="https://kaanwala.in/" target='_blank'>
+                      <img className="img-fluid bsb-scale bsb-hover-scale-up" loading="lazy" src={kaawalaProject} alt="Chain" />
+                    </a>
+                    <figcaption>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-eye text-white bsb-hover-fadeInDown" viewBox="0 0 16 16">
+                        <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
+                        <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
+                      </svg>
+                      <h4 className="h6 text-white bsb-hover-fadeInUp mt-2">View Project</h4>
+                    </figcaption>
+                  </figure>
+                  <div className="card-body border-0 bg-white p-4">
+                    <div className="entry-header mb-3">
+                      <h2 className="card-title entry-title h4 mb-0">
+                        <a className="link-dark link-opacity-100 link-opacity-75-hover text-decoration-none" href="https://kaanwala.in/" target='_blank'>Kaanwala Clinic</a>
+                      </h2>
+                    </div>
+                    <p className="card-text entry-summary text-secondary m-0 p-0 text-black" style={{ textAlign: 'justify' }}>
+                    Developed a website for Kaanwala Clinic using PHP. The website demonstrates the clinic services and also allows users to book appointments.
+                    </p>
+                  </div>
+                </div>
+              </article>
+            </div>
+            
             
             
             <div className="col-12 col-md-6 col-lg-4 d-flex">
@@ -297,6 +358,9 @@ const Projects = () => {
                 </div>
               </article>
             </div>
+
+           
+
           </div>
         </div>
       </section>
